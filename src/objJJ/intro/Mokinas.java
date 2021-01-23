d563933278761b2d596545d54da4670737f51202
Jurate <jurate.jastromskaite@gmail.com>
2021-01-23 14:47:52 +0200

    01.22 capsul

--- src/objJJ/intro/Mokinas.java
+++ src/objJJ/intro/Mokinas.java
@@ -2,20 +2,74 @@ package objJJ.intro;
 
 public class Mokinas {
 
-    public String name;  //null default
-    public int level;   //0  default
-    public String address;  //null default
+//    private String name;  //null default
+    private String firstName;
+    private String lastName;
+    private int level;   //0  default
+    private String address;  //null default
 
-    public Mokinas(String name) {
-        this.name = name;
+    public Mokinas(String firstName, String lastName) {
+        getName(firstName, lastName);
         this.level = 1;    // galima ir nurodyt this, bet nera
     }
-    public Mokinas(String name, int level) {
-        this.name = name;
+    public Mokinas(String firstName, String lastName, int level) {
+        this(firstName, lastName);
+//        if (name == null || name.length() <= 1) {
+//            throw new IllegalArgumentException("Neteisngas vardas");
+//        }
+    }
+    //Geteriai ir seteriai
+//    public String getName() {
+//        return name;
+//    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName == null || firstName.length() <=1) {
+            throw new IllegalArgumentException("Neteisngas vardas");
+        }
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName == null || lastName.length() <=1) {
+            throw new IllegalArgumentException("Neteisngas vardas");
+        }
+        this.lastName = lastName;
+    }
+
+//    public void setName(String name) {
+//        if (name == null || name.length() <=1) {
+//            throw new IllegalArgumentException("Neteisngas vardas");
+//        }
+//        this.name = name;
+//    }
+
+    //geteriai ir seteriai
+    public String getName() {
+        return firstName + " " + lastName;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
         this.level = level;
     }
-    public Mokinas() {
-        name="X";
-        address= "Babtai";
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
--- src/objJJ/p05/U1.java
+++ src/objJJ/p05/U1.java
@@ -5,19 +5,22 @@ import objJJ.intro.Mokinas;   // tik sita irasius galima nenaudot pilno kelio
 public class U1 {
     public static void main(String[] args) {
         System.out.println( U1.class.getName() );
-        Mokinas[] mokiniai = new Mokinas[] {
-                new Mokinas(),
-                new Mokinas( "Jurgis", 3 ),
-                new Mokinas( "Ona"),
-                new Mokinas( "JOnas", 1),
-                new Mokinas( "Antanas"),
-                new Mokinas( "Maryte", 2)
-        };
-//        for (Mokinas m : mokiniai) {
-//            System.out.println(m.name + " " + m.level + " " + m.address) ;
-//        }
-        mokiniai[0].name="Zose";
-        mokiniai[0].level=3;
+
+//        Mokinas[] mokiniai = new Mokinas[] {
+//                new Mokinas(),
+//                new Mokinas( "Jurgis", 3 ),
+//                new Mokinas( "Ona"),
+////                new Mokinas( "J", 1),
+//                new Mokinas( "Antanas"),
+//                new Mokinas( "Maryte", 2)
+//        };
+////        for (Mokinas m : mokiniai) {
+////            System.out.println(m.name + " " + m.level + " " + m.address) ;
+////        }
+//        mokiniai[0].name="Zose";
+//        mokiniai[0].level=3;
+
+        Mokinas[] mokiniai = readFromDB();
 
         System.out.println("----Pradiniai duomenys------");
         print(mokiniai);
@@ -45,13 +48,31 @@ public class U1 {
     }
     static void print(Mokinas[] mokiniai) {
         for (Mokinas m : mokiniai) {
-            System.out.println(m.name + " " + m.level) ;
+            System.out.println(m.getName() + " " + m.getLevel()) ;
         }
     }
     static int compare(Mokinas m1, Mokinas m2) {
-        if (m1.level < m2.level) return -1;
-        if (m1.level > m2.level) return 1;
+        if (m1.getLevel() < m2.getLevel()) return -1;
+        if (m1.getLevel() > m2.getLevel()) return 1;
         // jei abieju klases lygios
-        return m1.name.compareTo(m2.name);
+        return m1.getName().compareTo(m2.getName());
+    }
+    // fja nuskaitys is DB ir grazins mokiniu masyva
+    static Mokinas [] readFromDB () {
+        Mokinas[] mokiniai = new Mokinas[] {
+//                new Mokinas(),
+                new Mokinas( "Jurgis", "Juragaitis", 3 ),
+                new Mokinas( "Ona", "Onaite"),
+//                new Mokinas( "J", 1),
+                new Mokinas( "Antanas", "Antanaitis"),
+                new Mokinas( "Maryte", "Marytaite" ,2)
+        };
+//        mokiniai[0].setName(null); //"Zose";
+//        mokiniai[0].setLevel(0);
+
+
+
+        return mokiniai;
+
     }
 }
