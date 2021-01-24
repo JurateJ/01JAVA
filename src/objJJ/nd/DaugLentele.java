79a6990a91b60ac673f55f9495463c7b575ab1f1
Jurate <jurate.jastromskaite@gmail.com>
2021-01-24 15:26:45 +0200

    ND2 DaugLent su puse stulpeliu

--- src/objJJ/nd/DaugLentele.java
+++ src/objJJ/nd/DaugLentele.java
@@ -5,14 +5,34 @@ public class DaugLentele {
     public static void main(String[] args) {
         System.out.println("---Daugybos lentelė---");
         int a = 1;
-        for (int i = 1; i < 10; ++i) {
-
-            for (int j = 1; j < 10; j++) {
+        for (int i = 1; i < 11; ++i) {
+            for (int j = 1; j < 6; j++) {
                 int sandauga = a * j;
-                System.out.println( a + "x" + j + "=" + sandauga);
+                System.out.print( j + "x" + i + "=" + sandauga + " ");
             }
+            System.out.println( " ");
             a++;
         }
+
     }
 
 }
+
+
+//DAUG LENTELE 1 stulpeliu
+//public class DaugLentele {
+//
+//    public static void main(String[] args) {
+//        System.out.println("---Daugybos lentelė---");
+//        int a = 1;
+//        for (int i = 1; i < 10; ++i) {
+//
+//            for (int j = 1; j < 10; j++) {
+//                int sandauga = a * j;
+//                System.out.println( a + "x" + j + "=" + sandauga);
+//            }
+//            a++;
+//        }
+//    }
+
+
