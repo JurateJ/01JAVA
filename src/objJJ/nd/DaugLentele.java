c22effe1345bfb024ea30f02bd6f2e46dd20d05e
Jurate <jurate.jastromskaite@gmail.com>
2021-01-25 00:50:13 +0200

    ND2 su stulpeliais

--- src/objJJ/nd/DaugLentele.java
+++ src/objJJ/nd/DaugLentele.java
@@ -2,31 +2,32 @@ package objJJ.nd;
 
 public class DaugLentele {
 
-    public static void main(String[] args) {
-        System.out.println("---Daugybos lentelė---");
+    static void stulpeliai(int c, int d) {
         int a = 1;
-        for (int i = 1; i < 11; ++i) {
-            for (int j = 1; j < 6; j++) {
+        for (int i = 1; i <= 10; ++i) {
+            for (int j = c; j <= d; j++) {
                 int sandauga = a * j;
-                System.out.print( j + "x" + i + "=" + sandauga + " ");
+                System.out.print(j + "x" + i + "=" + sandauga + "\t");
             }
-            System.out.println( " ");
+            System.out.println("  ");
             a++;
         }
-
     }
-
+    public static void main(String[] args) {
+        System.out.println("--Daugybos lentelė--");
+        stulpeliai(1,5);
+        System.out.println( " ");
+        stulpeliai(6,10);
+    }
 }
 
-
-//DAUG LENTELE 1 stulpeliu
+//DAUGYBOS LENTELE 1 stulpeliu
 //public class DaugLentele {
 //
 //    public static void main(String[] args) {
 //        System.out.println("---Daugybos lentelė---");
 //        int a = 1;
 //        for (int i = 1; i < 10; ++i) {
-//
 //            for (int j = 1; j < 10; j++) {
 //                int sandauga = a * j;
 //                System.out.println( a + "x" + j + "=" + sandauga);
